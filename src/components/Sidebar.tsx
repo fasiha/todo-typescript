653be7391cd509f34163ea585d7429c5cea8e11d
satoshi-sh <satoss1108@gmail.com>
2023-05-04 13:10:22 -0500

    Add a side bar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import "./sidebar.css";
+import avatar from "../images/avatar.png";
+const UserHeader = () => {
+  return (
+    <div className="userheader">
+      <img src={avatar}></img>
+      <div className="userinfo">
+        <h3>User</h3>
+        <p>Free Plan</p>
+      </div>
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <>
+      <UserHeader />
+      <div className="sidebar">
+        <div className="mylist">
+          <h3>My Lists</h3>
+          <ul>
+            <li>Personal</li>
+            <li>Work</li>
+            <li>Hobby</li>
+          </ul>
+        </div>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Sidebar;
